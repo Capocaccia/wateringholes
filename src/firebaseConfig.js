c3693bb7beb2274ff780370a4c7fded4d00c5973
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-11-05 07:16:08 -0600

    removes api key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: 'AIzaSyBda8s1N7Vmlq6coof0zrVB2HAX7EwGBjM',
+    apiKey: '',
     databaseURL: 'https://wateringholes-6e315.firebaseio.com/',
     storageBucket: 'wateringholes-6e315'
 }
